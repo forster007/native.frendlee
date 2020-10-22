ae5bb39bf94a39bfed176d7cf629a8bbd9f1b498
Delmar Furtado <delmar.jr.furtado@gmail.com>
2020-10-22 10:54:05 -0300

    mid dev

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,4 @@
+import { FontAwesome } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -5,11 +6,14 @@ export const Container = styled.View`
   flex: 1;
 `;
 
-export const Content = styled.View`
+export const Content = styled.ScrollView.attrs({
+  keyboardShouldPersistTaps: 'always',
+  nestedScrollEnabled: true,
+  showsVerticalScrollIndicator: false,
+})`
   background-color: #ffffff;
   border-bottom-color: #f2f2f2;
   border-bottom-width: 1px;
-  flex: 1;
 `;
 
 export const ButtonConnectParent = styled.TouchableOpacity.attrs({
@@ -91,6 +95,7 @@ export const ButtonShareParent = styled.TouchableOpacity.attrs({
   justify-content: center;
   margin-top: 10px;
   width: 100%;
+  margin-bottom: 10px;
 `;
 
 export const ButtonShareParentText = styled.Text`
@@ -102,6 +107,7 @@ export const ButtonShareParentText = styled.Text`
 
 export const Div = styled.View`
   padding-horizontal: 10px;
+  background: #DCD9E3;
 `;
 
 export const Divisor = styled.View`
@@ -112,6 +118,13 @@ export const Divisor = styled.View`
   width: 100%;
 `;
 
+export const DivisorInfo = styled.View`
+  background-color: #CFCED8;
+  height: 1px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  width: 100%;
+`;
 export const Input = styled.TextInput`
   align-items: center;
   background-color: #f2f2f2;
@@ -150,3 +163,240 @@ export const ProfileNameText = styled.Text`
   font-weight: bold;
   padding-left: 10px;
 `;
+
+export const ProfileCardBiography = styled.View`
+  color: #2A3152;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+export const ProfileCardBiographyTitle = styled.Text`
+  color: #2A3152;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+export const ProfileCardBiographyText = styled.Text.attrs({
+  numberOfLines: 7,
+})`
+  color: #2A3152;
+  font-size: 16px;
+  line-height: 22.5px;
+`;
+
+export const ProfileCardInformation = styled.View`
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  border: 2px solid #E9E5EE;
+  border-radius: 5px;
+  height: 50px;
+  margin-top: 10px;
+`;
+
+export const ProfileCardInformationText = styled.Text`
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  color: #2A3152;
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 21px;
+  padding: 10px;
+`;
+
+export const ProfileCardInformationSsnIcon = styled(FontAwesome).attrs({
+  color: '#4C476F',
+  name: 'id-card-o',
+  size: 26
+})`
+  text-align: center;
+  padding: 0 10px;
+  width: 50px;
+`;
+
+export const ProfileCardInformationEmailIcon = styled(FontAwesome).attrs({
+  color: '#4C476F',
+  name: 'envelope-o',
+  size: 27
+})`
+  text-align: center;
+  padding: 0 10px;
+  width: 50px;
+`;
+
+export const ProfileCardInfoText = styled.Text`
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  color: #2A3152;
+  font-size: 18px;
+  font-weight: bold;
+  line-height: 21px;
+  padding: 10px;
+`;
+
+export const ProfileCardInfo = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 50px;
+  margin-top: 10px;
+  width: 100%;
+`;
+
+export const ProfileCardInfoAge = styled.View`
+  align-items: center;
+  border: 2px solid #E9E5EE;
+  border-radius: 5px;
+  height: 50px;
+  flex-direction: row;
+  justify-content: center;
+  width: 48%;
+`;
+
+export const ProfileCardInfoAgeIcon = styled.Image.attrs({
+  resizeMode: 'contain',
+  source: require('../../../assets/frendlee-provider-cake.png'),
+})`
+  height: 25px;
+  margin-right: 10px;
+  width: 25px;
+`;
+
+export const ProfileCardInfoAgeText = styled.Text`
+  color: #4c476f;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+export const ProfileCardInfoGender = styled.View`
+  align-items: center;
+  border: 2px solid #E9E5EE;
+  border-radius: 5px;
+  height: 50px;
+  flex-direction: row;
+  justify-content: center;
+  width: 48%;
+`;
+
+export const ProfileCardInfoGenderIconFemale = styled.Image.attrs(props => ({
+  resizeMode: 'contain',
+  source: require(`../../../assets/frendlee-gender-female.png`),
+  // source: require(`../../../assets/${props => props.gender === 'female' ? 'frendlee-gender-female.png' : 'frendlee-provider-male.png'}`),
+}))`
+  height: 25px;
+  margin-right: 10px;
+  width: 25px;
+`;
+
+export const ProfileCardInfoGenderIconMale = styled.Image.attrs({
+  resizeMode: 'contain',
+  source: require(`../../../assets/frendlee-provider-male.png`),
+})`
+  height: 25px;
+  margin-right: 10px;
+  width: 25px;
+`;
+
+export const ProfileCardInfoGenderText = styled.Text`
+  color: #4c476f;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+export const ProfileCardInfoPhone = styled.View`
+  align-items: center;
+  border: 2px solid #E9E5EE;
+  border-bottom-left-radius: 5px;
+  border-top-left-radius: 5px;
+  height: 50px;
+  flex-direction: row;
+  width: 65%;
+`;
+
+export const ProfileCardInfoPhoneIcon = styled(FontAwesome).attrs({
+  color: '#4C476F',
+  name: 'phone',
+  size: 26
+})`
+text-align: center;
+padding: 0 10px;
+width: 50px;
+`;
+
+export const ProfileCardInfoPhoneText = styled.Text`
+  color: #4c476f;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+export const ProfileCardInfoWhatsapp = styled.View`
+  align-items: center;
+  border: 2px solid #E9E5EE;
+  border-bottom-right-radius: 5px;
+  border-top-right-radius: 5px;
+  height: 50px;
+  flex-direction: row;
+  width: 35%;
+  background: #E9E5EE;
+`;
+
+export const ProfileCardInfoWhatsappIcon = styled(FontAwesome).attrs({
+  color: '#4C476F',
+  name: 'whatsapp',
+  size: 22
+})`
+  text-align: center;
+  width: 40px;
+`;
+
+export const ProfileCardInfoWhatsappText = styled.Text`
+  color: #4c476f;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const ProfileCardInfoMedical = styled.View`
+  color: #2A3152;
+  width: 100%;
+  margin: 20px 0;
+`;
+
+export const ProfileCardInfoMedicalTitle = styled.Text`
+  color: #2A3152;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+export const ProfileCardInfoMedicalItem = styled.View`
+  align-items: center;
+  border-radius: 2px;
+  height: 50px;
+  flex-direction: row;
+  flex: 1;
+  background: rgba(175, 174, 191, 0.5);
+  margin-top: 10px;
+`;
+
+export const ProfileCardInfoMedicalItemIcon = styled.Image.attrs({
+  resizeMode: 'contain',
+  source: require('../../../assets/frendlee-profile-medical.png'),
+})`
+  height: 25px;
+  margin: 10px;
+  width: 25px;
+`;
+
+export const ProfileCardInfoMedicalItemText = styled.Text`
+  color: #2A3152;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const ProfileCardInfoMedicalFlatList = styled.FlatList``;
